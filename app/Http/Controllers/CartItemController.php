0a9e072ea926b4d7f7a6eba8a7d7db213ca29535
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-08-26 22:39:58 +0700

    refactor [scheme]: added cart and cart item schemes

---
+++ app/Http/Controllers/CartItemController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CartItem;
+use App\Http\Requests\StoreCartItemRequest;
+use App\Http\Requests\UpdateCartItemRequest;
+
+class CartItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCartItemRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CartItem $cartItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCartItemRequest $request, CartItem $cartItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CartItem $cartItem)
+    {
+        //
+    }
+}
